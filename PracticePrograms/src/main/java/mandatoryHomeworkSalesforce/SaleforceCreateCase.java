678d8623e89161e26b50f3d586150583fbbc8699
archanadharsini <archanadharsini@localhost>
2023-09-08 06:17:49 +0530

    commit 2

---
+++ PracticePrograms/src/main/java/mandatoryHomeworkSalesforce/SaleforceCreateCase.java
@@ -0,0 +1,78 @@
+package mandatoryHomeworkSalesforce;
+
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class SaleforceCreateCase {
+
+    public static void main(String[] args) {
+       
+    	WebDriverManager.chromedriver().setup();
+
+        
+        WebDriver driver = new ChromeDriver();
+
+        
+        driver.manage().window().maximize();
+
+        
+        driver.get("https://login.salesforce.com");
+
+        
+
+        WebElement usernameInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("Login"));
+
+        usernameInput.sendKeys("dharsinieee@gmail.com");
+        passwordInput.sendKeys("Archanadharsini07*");
+        loginButton.click();
+
+       
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        WebElement globalActionsIcon = driver.findElement(By.cssSelector("button[title='Global Actions']"));
+        globalActionsIcon.click();
+
+        
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        WebElement newCaseButton = driver.findElement(By.xpath("//a[@title='New Case']"));
+        newCaseButton.click();
+
+        
+        WebElement contactNameDropdown = driver.findElement(By.xpath("//select[@title='Contact Name']"));
+        Select contactNameSelect = new Select(contactNameDropdown);
+        contactNameSelect.selectByVisibleText("Archana Dharsini "); 
+
+       
+        WebElement statusDropdown = driver.findElement(By.xpath("//select[@title='Status']"));
+        Select statusSelect = new Select(statusDropdown);
+        statusSelect.selectByVisibleText("Escalated");
+
+        
+        WebElement subjectInput = driver.findElement(By.xpath("//input[@title='Subject']"));
+        WebElement descriptionInput = driver.findElement(By.xpath("//textarea[@title='Description']"));
+
+        subjectInput.sendKeys("Testing");
+        descriptionInput.sendKeys("Dummy");
+
+        
+        WebElement saveButton = driver.findElement(By.xpath("//button[@title='Save']"));
+        saveButton.click();
+
+       
+        driver.quit();
+    }
+}
---
+++ PracticePrograms/src/main/java/mandatoryHomeworkSalesforce/SalesforceEditCase.java
@@ -0,0 +1,113 @@
+package mandatoryHomeworkSalesforce;
+
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SalesforceEditCase {
+    public static void main(String[] args) {
+        
+    	WebDriverManager.chromedriver().setup();
+
+        
+        WebDriver driver = new ChromeDriver();
+
+        
+        driver.get("https://login.salesforce.com");
+        driver.findElement(By.id("username")).sendKeys("dharsinieee@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("Archanadharsini07*");
+        driver.findElement(By.id("Login")).click();
+
+        
+        driver.findElement(By.className("slds-icon-waffle")).click();
+
+       
+
+        driver.findElement(By.xpath("//button[text()='View All']")).click();
+        driver.findElement(By.xpath("//p[text()='Sales']")).click();
+        
+        try {
+            Thread.sleep(3000); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        
+        
+     
+        WebElement moreTab = driver.findElement(By.xpath("//a[@title='More']"));
+        moreTab.click();
+
+       
+        WebElement dropdownIcon = driver.findElement(By.xpath("//a[@title='Show Navigation Menu']"));
+        dropdownIcon.click();
+
+        WebElement casesOption = driver.findElement(By.xpath("//a[@title='Cases']"));
+        casesOption.click();
+        
+     
+        try {
+            Thread.sleep(3000); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+    
+
+        WebElement casesTab = driver.findElement(By.xpath("//a[@title='Cases Tab']"));
+        casesTab.click();
+
+        
+        try {
+            Thread.sleep(3000); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        
+        
+        
+        
+
+       
+
+       String caseOwnerAlias = "ADhar"; 
+        WebElement caseDropdown = driver.findElement(By.xpath("//a[contains(@title, '" + caseOwnerAlias + "')]"));
+       caseDropdown.click();
+       driver.findElement(By.xpath("//a[text()='Edit']")).click();
+
+       
+
+        WebElement statusDropdown = driver.findElement(By.xpath("//span[text()='Status']/parent::label/following-sibling::div//a"));
+        statusDropdown.click();
+        driver.findElement(By.xpath("//a[text()='Working']")).click();
+
+        
+
+        WebElement priorityDropdown = driver.findElement(By.xpath("//span[text()='Priority']/parent::label/following-sibling::div//a"));
+        priorityDropdown.click();
+        driver.findElement(By.xpath("//a[text()='Low']")).click();
+
+        
+        WebElement caseOriginDropdown = driver.findElement(By.xpath("//span[text()='Case Origin']/parent::label/following-sibling::div//a"));
+        caseOriginDropdown.click();
+        driver.findElement(By.xpath("//a[text()='Phone']")).click();
+
+        
+        WebElement slaViolationDropdown = driver.findElement(By.xpath("//span[text()='SLA Violation']/parent::label/following-sibling::div//a"));
+        slaViolationDropdown.click();
+        driver.findElement(By.xpath("//a[text()='No']")).click();
+
+        
+        driver.findElement(By.xpath("//button[text()='Save']")).click();
+
+        
+        driver.quit();
+    }
+}
